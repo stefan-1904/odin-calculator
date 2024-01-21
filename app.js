ee9642a77b9e08c3c341ac622120939f44eaded4
Stefan <sminns1230@gmail.com>
2024-01-21 22:02:16 +0000

    Added styling to calculator

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
 const buttons = document.querySelectorAll('button');
 const calculatorDisplay = document.getElementById('display');
 
-function buttonHandler() {
-    buttons.forEach(function(button) {
-        button.addEventListener('click', () => {
-            let displayValue = calculatorDisplay.textContent += button.innerText;
-            return displayValue;
-        });
-    });
+// Variables to store calculator state
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+// Updates display with current user input
+function updateDisplay() {
+    calculatorDisplay.textContent = num1 + operator + num2;
 }
 
-buttonHandler()
+buttons.forEach(function(button) {
+    button.addEventListener('click', () => {
+        const value = button.dataset.value;
 
+        if(!isNaN(value)) {
+            if (!operator) {
+                // If operator has yet to be chosen, update first number
+                num1 += value;
+            } else {
+                // If operator has been chosen, update second number
+                num2 += value;
+            }
+        }
 
+        if (['+', '-', '*', '/',].includes(value)) {
+            // Check if operator already selected
+            if (operator) {
+                // Calculate result if both numbers and operator are present
+                num1 = operate(operator, parseFloat(num1), parseFloat(num2));
+                num2 = ''; // Reset number 2 for the next calculation
+            }
+            operator = value; // Set the new operator
+        }
+
+        if (value === '=') {
+            if(num1 && operator && num2) {
+                // Perform calculation & update state
+                num1 = operate(operator, parseFloat(num1), parseFloat(num2));
+                num2 = ''; // Reset num2 for next calculation
+                operator = ''; // Reset operator
+            }
+        }
+
+        if (value === 'clear') {
+            num1 = '';
+            num2 = '';
+            operator = '';
+        }
+
+        updateDisplay();
+    });
+});
 
 function add(num1, num2) {
     return num1 + num2;
@@ -27,6 +67,9 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if (num2 === 0) {
+        return 'Error';
+    }
     return num1 / num2;
 }
 
@@ -44,7 +87,3 @@ function operate(operator, num1, num2) {
             console.log('Invalid operator');
     }
 }
-
-let num1 = 10
-let num2 = 2
-let operator = '/';
